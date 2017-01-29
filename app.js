e67d10043898b08abfcad998358f8e3048235a22
Hitesh03011999 <hiteshpardasani99@gmail.com>
2017-01-29 22:34:41 +0530

    Added firebase

---
+++ app.js
@@ -0,0 +1,69 @@
+/*console.log('Hello World');
+var name='Hitesh';
+console.log(name);
+var n = 5;
+if(n<5){
+  console.log("n is less than 5");
+}
+else{
+  console.log("n is either equal to or greater than 5");
+}
+var user = 'Hitesh0301';
+function sayHello(name){
+  console.log('Hello '+ name);
+  console.log(user);
+}
+sayHello(5);
+function dowork(work) {
+  work();
+}
+dowork(function() {
+  console.log("Working");
+});
+//DOM is an object oriented representation of HTML document.
+console.log(document);
+var logoElements= document.getElementsByClassName('logo');
+var logo=logoElements[0];
+logo.textContent="Hello";
+var logoname=logo.textContent;
+console.log(logoname);*/
+
+click("signin-button",logInUser);
+// var user1 = {
+//   name: 'Hitesh',
+//   id : 1,
+//   username: 'Hitesh0301',
+//   sayHello: function(){
+//     console.log('Hello' + this.name);
+//   }
+// };
+// var user2 = {
+//   name: 'Hitesh2',
+//   id : 2,
+//   username: 'Hitesh0301 2',
+//   sayHello: function(){
+//     console.log('Hello' + this.name);
+//   }
+// };
+// var users = [
+//   user1,
+//   user2
+// ];
+// console.log(users[0]);
+// for (i=0;i<users.length;i++)
+// {var user = users[i];
+//   console.log(user.id);
+// }
+// var message = new Object();
+// message.text = 'Hello';
+// console.log(message.text);
+
+// Get a reference to the database service
+
+// usersRef.child("1234").set({
+// email: "hiteshpardasani@gmail.com"
+// }
+// );
+// usersRef.on('value',function (snapshot) {
+//   console.log(snapshot.val());
+// });
